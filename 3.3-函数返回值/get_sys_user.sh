5f0ccbc48b5c5c4bc697488559669b1744ff5e81
酷田 <522412857@qq.com>
2019-01-28 17:31:30 +0800

    上传文件至 '3.3-函数返回值'

---
+++ 3.3-函数返回值/get_sys_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+function get_users
+{
+	users=`cat /etc/passwd | cut -d: -f1`
+	echo $users
+}
+
+user_list=`get_users`
+
+index=1
+for u in $user_list
+do
+	echo "The $index user is : $u"
+	index=$(($index+1))
+done
