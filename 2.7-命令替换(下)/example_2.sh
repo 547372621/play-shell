55a2992e66dba7f7ecc31e672ad9b9e8989d5171
酷田 <522412857@qq.com>
2019-01-28 17:29:27 +0800

    上传文件至 '2.7-命令替换(下)'

---
+++ 2.7-命令替换(下)/example_2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+echo "This year have passed $(date +%j) days"
+echo "This year have passed $(($(date +%j)/7)) weeks"
+
+echo "There is $((365 - $(date +%j))) days before new year"
+echo "There is $(((365 - $(date +%j))/7)) weeks before new year"
